5fb3de6546cc99007f51370e691641822c3c882c
Parth Sodha <parthsodha2650@gmail.com>
2025-10-25 22:23:34 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,39 @@
+// let texth1 = document.querySelector(".text h1");
+// let btn1 = document.querySelector(".btn1");
+// let btn2 = document.querySelector(".btn2");
+// let body = document.querySelector("body");
+
+// btn1.addEventListener("mousemove", function () {
+//   btn1.style.backgroundColor = "#ecb76cff";
+//   btn1.style.cursor = "pointer";
+//   // texth1.style.webkitTextStroke = "2px red" ;
+// });
+// btn1.addEventListener("mouseleave", function () {
+//   btn1.style.backgroundColor = "#f69d1f";
+//   btn1 = "pointer";
+// });
+
+// btn2.addEventListener("mousemove", function () {
+//   btn2.style.backgroundColor = "aliceblue";
+//   btn2.style.cursor = "pointer";
+//   btn2.style.color = "black";
+//   // texth1.style.webkitTextStroke = "2px red" ;
+// });
+
+// btn2.addEventListener("mouseleave", function () {
+//   btn2.style.backgroundColor = "#3e141e";
+//   btn2.style.color = "aliceblue";
+//   btn2.style.cursor = "pointer";
+// });
+
+// btn1.addEventListener("click", function () {
+//   btn1.style.backgroundColor = "#865d23ff";
+//   btn1.style.cursor = "pointer";
+//   body.style.backgroundColor = "#57353dff";
+//   texth1.style.textShadow = "0 0 5px red";
+
+//   setTimeout(() => {
+//     body.style.backgroundColor = "";
+//     texth1.style.textShadow = "";
+//   }, 1000);
+// });
